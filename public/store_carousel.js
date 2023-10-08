7c1af4749e2581152af912cb036572e67dbb79fe
Amine <amkebichi@gmail.com>
2023-10-08 11:19:17 -0700

    added cylical functionality to carousel

--- public/store_carousel.js
+++ public/store_carousel.js
@@ -7,15 +7,21 @@ let currentSlide = 0;
 prevButton.addEventListener('click', () => {
   if (currentSlide > 0) {
     currentSlide--;
-    updateCarousel();
+  } else {
+    // If already at the beginning, wrap around to the end.
+    currentSlide = (content.children.length / 2) - 1;
   }
+  updateCarousel();
 });
 
 nextButton.addEventListener('click', () => {
   if (currentSlide < (content.children.length / 2) - 1) {
     currentSlide++;
-    updateCarousel();
+  } else {
+    // If already at the end, wrap around to the beginning.
+    currentSlide = 0;
   }
+  updateCarousel();
 });
 
 function updateCarousel() {
