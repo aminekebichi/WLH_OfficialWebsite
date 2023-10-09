3a1f09d132dd5d83139d2aac48fb4270619e4e4d
Amine <amkebichi@gmail.com>
2023-10-08 21:08:17 -0700

    updated carousel functionality

--- public/store_carousel.js
+++ public/store_carousel.js
@@ -1,24 +1,28 @@
 const btn_width = 200; //pixels
-const margin_width = 20; //pixels
+const item_margins = 20; // pixels
 
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const content = document.querySelector('.carousel-content');
 
+let visibleItems = 3;
 let currentSlide = 0;
+
+document.documentElement.style.setProperty('--visibleItems', visibleItems);
+document.documentElement.style.setProperty('--btn_width', btn_width);
 
 prevButton.addEventListener('click', () => {
     if (currentSlide > 0) {
       currentSlide--;
     } else {
       // If already at the beginning, wrap around to the end.
-      currentSlide = (content.children.length / 2) - 1;
+      currentSlide = content.children.length - 1;
     }
     updateCarousel();
   });
   
   nextButton.addEventListener('click', () => {
-    if (currentSlide < (content.children.length / 2) - 1) {
+    if (currentSlide < (content.children.length) - 1) {
       currentSlide++;
     } else {
       // If already at the end, wrap around to the beginning.
@@ -29,9 +33,6 @@ prevButton.addEventListener('click', () => {
   
   function updateCarousel() {
     const flexboxWidth = window.innerWidth;
-    // Calculate the total margin which is 2 (left and right) * 20px * number of elements
-    const totalMargin = 2 * 20 * content.children.length;
-
-    const offset = -currentSlide*((flexboxWidth - totalMargin) / content.children.length);
+    const offset = -currentSlide*((flexboxWidth - 2*btn_width) / visibleItems + item_margins);
     content.style.transform = `translateX(${offset}px)`;
   }
